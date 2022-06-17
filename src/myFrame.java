d783fc14f42e8dbfc12d63ef7a5590d756fc7318
Ishan-Rawat <rawatishan03@gmail.com>
2022-06-17 17:15:51 +0530

    Initial commit

---
+++ src/myFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class myFrame extends JFrame{
+	myFrame(){
+		this.setSize(500, 500);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Countdown timer");
+		
+	}
+}
