8481f1690c80d86e6b43049e85938308f97dbea7
Ishan-Rawat <rawatishan03@gmail.com>
2022-06-17 23:49:17 +0530

    added time increase button

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.color.*;
 import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// abhi problem yeh hai ki JButton ko myFrame mai implement kar diya toh intTime ko modify kaise karunga. 
+// Aur JButton is class mai implement ho nahi raha hai
 
 public class Main {
+	JButton upButton;
+	static int intTime = 60;
+	
 	public static void main(String args[]) {
 		myFrame frame = new myFrame();
 		
@@ -12,7 +22,7 @@ public class Main {
 		mainPanel.setBackground(new Color(41,19,46));
 		
 		//creating the Countdown timer's time showing part's label:
-		int intTime = 60;
+		
 		String stringTime= String.valueOf(intTime);
 		
 		JLabel time = new JLabel();
@@ -21,6 +31,10 @@ public class Main {
 		time.setVerticalTextPosition(JLabel.CENTER);
 		time.setFont(new Font("Helvetica", Font.PLAIN, 50));
 		time.setForeground(new Color(222,0,42));
+		
+		//Creating the up and down buttons
+		
+		ImageIcon down = new ImageIcon("C:\\Users\\ishan\\eclipse-workspace\\Countdown Timer\\down.png");
 		
 		mainPanel.add(time);
 		
--- src/myFrame.java
+++ src/myFrame.java
@@ -1,11 +1,36 @@
 import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
-public class myFrame extends JFrame{
+public class myFrame extends JFrame implements ActionListener{
+	JButton upButton;
 	myFrame(){
 		this.setSize(500, 500);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("Countdown timer");
 		
+		upButton = new JButton();
+		upButton.addActionListener(this);
+		upButton.setFocusable(false);
+		upButton.setBackground(new Color(50,20,80));
+		upButton.setBounds(10, 10, 100, 50);
+		
+		ImageIcon up = new ImageIcon("C:\\Users\\ishan\\eclipse-workspace\\Countdown Timer\\up.png");
+		upButton.setIcon(up);
+		
+		this.add(upButton);
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==upButton) {
+			//System.out.println("Button clicked!");
+			Main.intTime++;
+			System.out.println(Main.intTime);
+		}
 	}
 }
